4e0e767e341487409d48c32034b5546352f549e7
Pawel Grynienko <pgrynienko@objectivity.co.uk>
2017-11-29 10:59:54 +0100

    first init

---
+++ index.android.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Root from './App/Root';
+
+AppRegistry.registerComponent(
+  'reactNativeBeaconExample',
+  () => Root
+);
